7de4f9d435a8c5fd0d6cb2be676e6cd8b9a6bc04
Rakib <rakibulhasan3929@gmail.com>
2024-06-04 17:06:53 +0600

    update a code

--- src/Pagse/Home/Packge.jsx
+++ src/Pagse/Home/Packge.jsx
@@ -16,14 +16,17 @@ const Package = () => {
 
     return (
         <div>
-            <div className="grid md:grid-cols-3 mt-12 gap-10">
+            {/* <div className="grid md:grid-cols-3 mt-12 gap-10">
                 {data.map(item => (
                     <div key={item.id} className="card lg:h-[500px] bg-base-100 shadow-xl">
                         <figure><img className="" src={item.images} alt="Food" /></figure>
-                        <p className="absolute bottom-48 right-2 px-4 bg-red-500 text-white">{item.price}</p>
+                        <p className="absolute bottom-52 right-2 px-4 bg-red-500 text-white">{item.price}</p>
                         <div className="card-body flex flex-col items-center">
                             <h2 className="card-title">{item.trip_title}</h2>
-                            <p>{item.tour_type}</p>
+                          
+                            <div className="divider divider-primary">
+                            <p className="text-xl">{item.tour_type}</p>
+                            </div>
                             <div className="card-actions justify-end">
                                 <button
                                     // onClick={() => handleAddtoCart(item)}
@@ -36,11 +39,36 @@ const Package = () => {
                     </div>
                 ))}
 
+            </div> */}
+            <div className="grid md:grid-cols-3 gap-10">
+                {
+                    data.map(item =>
+                        <div key={item._id} className=" p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-900">
+                            <img src={item.images} alt="" className="object-cover object-center w-full rounded-md h-72 dark:bg-gray-500" />
+                            <div className="mt-6 mb-2">
+                                <h2 className="text-center text-2xl">{item.trip_title}</h2>
+                                <div className="divider divider-success">
+                                    <p>{item.tour_type}</p>
+                                </div>
+                                <div className="card-actions justify-center mt-8">
+                                    <button
+                                        // onClick={() => handleAddtoCart(item)}
+                                        className="btn btn-outline bg-slate-300 btn-accent border-0 border-b-4"
+                                    >
+                                        view Package
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    )
+                }
             </div>
             <div className="mt-12 mb-8">
-              <Link to= '/package'>
-              <button className="btn btn-outline text-white btn-success ">All Packages</button>
-              </Link>
+                <Link to='/package'>
+                    <button className="btn btn-outline text-white btn-success ">All Packages</button>
+                </Link>
             </div>
         </div>
     );
